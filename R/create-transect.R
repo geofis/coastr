bdf2a49f1d6ecd6f795359293f262899570a5f76
Jose Ramon <joseramon@geografiafisica.org>
2020-06-27 15:52:27 -0400

    create_transect function now accepts different length for each side, as well as reverse capability

--- R/create-transect.R
+++ R/create-transect.R
@@ -5,9 +5,14 @@
 #' @param x name of an object of class \code{sf}, or a path to a file
 #'   readable by the \code{sf::st_read} function, which must contain
 #'   geometries of type \code{LINESTRING} or \code{MULTILINESTRING}.
-#' @param transect_spacing numeric; spacing between transects
-#' @param length_left numeric; length of transects to the left of the input line
+#' @param transect_spacing character or numeric. If numeric, represents
+#'   the spacing between transects in \code{x} CRS units. If character,
+#'   accepts only the string 'vertices' as argument (allows partial matching)
+#' @param length_left numeric; length of transects to the left
+#'   of the input line. Default to \code{transect_spacing}
 #' @param length_left numeric; length of transects to the right of the input line
+#' @param reverse logical; reverse orientation of
+#'   transects; default \code{FALSE}
 #' @param ... other arguments passed to sf::st_read.
 #' @details
 #'
@@ -15,38 +20,58 @@
 #'
 #' @examples
 #'
-#' @importFrom magrittr `%>%`
+#' @importFrom assertthat assert_that
 #' @importFrom sf st_read st_coordinates st_geometry st_geometry_type st_line_sample st_cast st_crs `st_crs<-` st_sf st_sfc st_linestring st_multilinestring
-#' @importFrom zoo zoo
 #' @export
-create_transect <- function(x, transect_spacing = NULL,
-                            length_left = if(is.null(transect_spacing))
-                              NULL else transect_spacing,
-                            length_right = if(is.null(transect_spacing))
-                              NULL else transect_spacing, ...) {
-  if(is.character(x)) {
-    obj <- invisible(st_read(dsn = x, quiet = T, ...))
-  } else {
-    if(any(class(x) %in% 'sf')) {
-      obj <- x
-      } else {
-        stop('x must be a character string containing a path to a source
-         or the name of an object of class sf')
-      }
-  }
+create_transect <- function(
+  x, transect_spacing = NULL,
+  length_left = if(
+    all(!is.null(transect_spacing), is.numeric(transect_spacing)))
+    transect_spacing,
+  length_right = if(
+    all(!is.null(transect_spacing), is.numeric(transect_spacing)))
+    transect_spacing,
+  reverse = FALSE, ...) {
+  assert_that(
+    any(
+      all(is.character(transect_spacing), grepl(transect_spacing, 'vertices')),
+      all(is.numeric(transect_spacing), transect_spacing > 0)),
+    msg = 'transect_spacing must be "vertices" or a value greater than zero'
+  )
+  assert_that(
+    !is.null(length_left), !is.null(length_right),
+    msg = 'must set values for both length_left and length_right')
+  assert_that(
+    length_left >= 0, length_right >= 0,
+    msg = 'length_left and length_right must be greater or equal than zero'
+  )
+  obj <- create_obj(x)
   check_object(obj)
   point <- create_point(obj, transect_spacing)
   coords <- calculate_xy_coords(point)
   lag <- calculate_lag(coords)
   angle <- calculate_mean_angle(lag)
   point_coordinates <- create_point_coordinates(angle, length_left, length_right)
-  point_sides <- find_side_of_points(point_coordinates)
-  transects_sides <- create_transect_sides(point_sides, sides)
-  return(transects_sides)
+  transects <- create_multilinestring(point_coordinates, length_left, length_right, reverse)
+  return(transects)
 }
 
-create_point <- function(obj, transect_spacing = transect_spacing) {
-  if(is.null(transect_spacing)) {
+create_obj <- function(x, ...) {
+  if(is.character(x)) {
+    obj <- invisible(st_read(dsn = x, quiet = T, ...))
+  } else {
+    if(any(class(x) %in% 'sf')) {
+      obj <- x
+    } else {
+      stop('x must be a character string containing a path to a source
+         or the name of an object of class sf')
+    }
+  }
+  return(obj)
+}
+
+create_point <- function(obj, transect_spacing) {
+  if(grepl(transect_spacing, 'vertices')) {
     point_vertex <- suppressWarnings(st_cast(obj, 'POINT'))
     point <- st_geometry(point_vertex)
   } else {
@@ -134,107 +159,23 @@ create_point_coordinates <- function(angle, length_left, length_right){
   return(point_coords)
 }
 
-create_multilinestring <- function(point_coordinates, length_left, length_right) {
-  if(all(!is.null(length_left), !is.null(length_right))) {
-    linestring_list <- lapply(1:nrow(point_coordinates), function(h) {
-      linestring_sfg <- st_linestring(
-        with(
-          point_coordinates[h, ],
-          rbind(c(x_left, y_left), c(x_right, y_right))
-        )
+create_multilinestring <- function(point_coordinates, length_left, length_right, reverse) {
+  linestring_list <- lapply(1:nrow(point_coordinates), function(h) {
+    linestring_sfg <- st_linestring(
+      with(
+        point_coordinates[h, ],
+        if(reverse)
+          rbind(c(x_right, y_right), c(x_left, y_left))
+        else rbind(c(x_left, y_left), c(x_right, y_right))
       )
-      linestring_sfc <- st_sfc(linestring_sfg)
-      linestring_sf <- st_sf(
-        data.frame(coastr_id = h, type = 'across'),
-        geometry = linestring_sfc)
-      return(linestring_sf)
-    })
-    linestring <- do.call('rbind', linestring_list)
-  } else {
-    if(is.null(length_right)) {
-      
-    } else {
-      if (is.null(length_left)) {
-        
-      }
-    }
-  }
-  st_crs(linestring) <- attributes(point_coordinates)$epsg
-  return(linestring)
-}
-#
-
-
-
-
-
-
-
-
-
-
-
-find_side_of_points <- function(point_coordinates) {
-  coastr_id_index <- unique(point_coordinates$coastr_id)
-  sides_list <- lapply(coastr_id_index, function(z) {
-    sign1 <- with(point_coordinates,
-                  ( (x[coastr_id == ifelse(z==length(coastr_id_index), z, z + 1) & index == 0] - x[coastr_id == ifelse(z==length(coastr_id_index), z-1, z) & index == 0 ]) *
-                      (y[coastr_id == z & index == 1] - y[coastr_id == ifelse(z==length(coastr_id_index), z-1, z) & index == 0 ]) -
-                      (y[coastr_id == ifelse(z==length(coastr_id_index), z, z + 1) & index == 0 ] - y[coastr_id == ifelse(z==length(coastr_id_index), z-1, z) & index == 0 ]) *
-                      (x[coastr_id == z & index == 1] - x[coastr_id == ifelse(z==length(coastr_id_index), z-1, z) & index == 0 ]) )
     )
-    side1 <- ifelse(sign1 < 0, 'right', ifelse(sign1 > 0, 'left', 'on the line'))
-    sign2 <- with(point_coordinates,
-                  ( (x[coastr_id == ifelse(z==length(coastr_id_index), z, z + 1) & index == 0] - x[coastr_id == ifelse(z==length(coastr_id_index), z-1, z) & index == 0 ]) *
-                      (y[coastr_id == z & index == 2] - y[coastr_id == ifelse(z==length(coastr_id_index), z-1, z) & index == 0 ]) -
-                      (y[coastr_id == ifelse(z==length(coastr_id_index), z, z + 1) & index == 0 ] - y[coastr_id == ifelse(z==length(coastr_id_index), z-1, z) & index == 0 ]) *
-                      (x[coastr_id == z & index == 2] - x[coastr_id == ifelse(z==length(coastr_id_index), z-1, z) & index == 0 ]) )
-    )
-    side2 <- ifelse(sign2 < 0, 'right', ifelse(sign2 > 0, 'left', 'on the line'))
-    sides_df <- data.frame(coastr_id = rep(z, 3), index = c(0, 1, 2),
-                        side = c('on the line', side1, side2))
-    return(sides_df)
-  })
-  sides_rbind <- do.call(rbind, sides_list)
-  sides <- base::merge(point_coordinates, sides_rbind)
-  attr(sides, 'epsg') <- attributes(point_coordinates)$epsg
-  class(sides) <- c('sides_df', class(point_coordinates))
-  return(sides)
-}
-
-create_transect_sides <- function(sides_df, sides) {
-  if(is.null(sides))
-    stop('argument sides must be a valid character string')
-  if(!any(sides == 'left', sides == 'right', sides == 'across'))
-    stop('argument sides must be a character string of value
-         "left", "right", or "across"')
-  linestring_list <- lapply(unique(sides_df$coastr_id), function(i) {
-    if(sides == 'across') {
-      j <- 'right'
-      k <- 'left'
-    } else {
-      j <- sides
-      k <- 'on the line'
-    }
-    linestring_side_list <- lapply(j, function(j) {
-      linestring_sfg <- st_linestring(
-        with(
-          sides_df[sides_df$coastr_id==i, ],
-          rbind(c(x[side==j], y[side==j]), c(x[side==k], y[side==k]))
-        )
-      )
-      linestring_sfc <- st_sfc(linestring_sfg)
-      linestring_sf <- st_sf(
-        data.frame(
-          coastr_id = i,
-          side = ifelse(sides=='across', 'across', j)),
-        geometry = linestring_sfc)
-      return(linestring_sf)
-    })
-    linestring_side <- do.call('rbind', linestring_side_list)
-    return(linestring_side)
+    linestring_sfc <- st_sfc(linestring_sfg)
+    linestring_sf <- st_sf(
+      data.frame(coastr_id = h),
+      geometry = linestring_sfc)
+    return(linestring_sf)
   })
   linestring <- do.call('rbind', linestring_list)
-  st_crs(linestring) <- attributes(sides_df)$epsg
+  st_crs(linestring) <- attributes(point_coordinates)$epsg
   return(linestring)
 }
